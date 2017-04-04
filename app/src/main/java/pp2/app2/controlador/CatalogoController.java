b9964d6f8ac533df9a81569d1c5c3399bf6e782e
Fernanda Cazzari Tusquet <Fernanda Cazzari Tusquet>
2017-04-04 12:13:57 -0300

    Al fin

--- app/src/main/java/pp2/app2/controlador/CatalogoController.java
+++ app/src/main/java/pp2/app2/controlador/CatalogoController.java
@@ -1,7 +1,12 @@
 package pp2.app2.controlador;
 
 import android.content.res.Resources;
+import android.os.Message;
+import android.util.JsonReader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
